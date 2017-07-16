107581840de279d8ad81e7a68eab35a46e64d4b2
Kathryn Rowe <kjrowe06@gmail.com>
2017-07-16 16:40:38 -0700

    Adding unittests

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_searches(self):
+
+		#opens homepage
+		self.browser.get('http://localhost:8000')
+
+		#Begining tests, starting up Django app
+		assert 'Django' in browser.title
+
+		#Homepage gives explanation about what it does
+		self.assertIn('Medication', self.browser.title)
+		self.fail('Finish the test!')
+
+		#Search for a drug concept ex Search for alavert
+
+		#Drug concepts will appear on the page
+
+		#Select a particular drug concept to serve as the reference drug ex Select Alavert 10 MG Oral Tablet .
+
+		#Return a list of all generic and branded drugs that contain the same active ingredients as a reference drug
+
+		#List groups in generic and branded
+
+		#Would be great for app to describe what this group of meds does
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
