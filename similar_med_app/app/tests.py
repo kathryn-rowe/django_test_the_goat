20052a5abf8772114b5fbfdcd8ba472b359404eb
Kathryn Rowe <kjrowe06@gmail.com>
2017-07-29 17:25:24 -0700

    Adding new models

--- similar_med_app/app/tests.py
+++ similar_med_app/app/tests.py
@@ -1,36 +1,28 @@
-from django.urls import resolve
+
 from django.test import TestCase
-from django.http import HttpRequest
+from app.models import Item, List
 
-from app.models import Item
-
-from app.views import home_page
 
 
 class HomePageTest(TestCase):
 
 	def test_uses_home_template(self):
-		
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
 
-		response = self.client.post('/', data={'item_text': 'A new list item'})
+class NewListTest(TestCase):
+	
+	def test_can_save_a_POST_request(self):
+		self.client.post('/app/new', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
+
 	def test_redirects_after_POST(self):
-
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/app/the-only-list-in-the-world/')
-
-	def test_only_saves_items_when_necessary(self):
-		
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
+		response = self.client.post('/app/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/app/the-only-list-in-the-world/')
 
 
 class ListViewTest(TestCase):
@@ -39,27 +31,30 @@ class ListViewTest(TestCase):
 		response = self.client.get('/app/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
 
-	
+
 	def test_displays_all_list_items(self):
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-
-		response = self.client.get('/app/the-only-list-in-the-world/')
-
-		self.assertContains(response, 'itemey 1')
-		self.assertContains(response, 'itemey 2')
-
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -67,4 +62,6 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
